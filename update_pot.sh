e3c8e0ba1aab4b5358205c44527fc5db2d455f7a
Remi Rampin <r@remirampin.com>
2019-10-12 15:02:47 -0400

    Simplify script a little

--- update_pot.sh
+++ update_pot.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-set -eu
-
-pybabel extract -F babelrc -k Label/text -k tr -o godot-l10n.pot $(find . -name '*.tscn' -o -name '*.gd')
+pybabel extract -F babelrc -k Label/text -k tr -o godot-l10n.pot .
